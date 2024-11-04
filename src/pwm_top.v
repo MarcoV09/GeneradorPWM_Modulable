51f0f4f0dc16c3926b7e302a776c983dcd46dbdc
MarcoV09 <138732788+MarcoV09@users.noreply.github.com>
2024-11-04 10:04:02 -0700

    Update pwm_top.v

--- src/pwm_top.v
+++ src/pwm_top.v
@@ -12,18 +12,24 @@ module tt_um_pwm_top(
 );
 
     // All output pins must be assigned. If not used, assign to 0.
-    assign uo_out[7:1] = 0;    // only uo_out[0] is used as output for pwm_out
     assign uio_out[7:0] = 0;        
     assign uio_oe[7:0]  = 0;
     
     //List all unused inputs to prevent warnings
-    wire _unused = &{ena, uo_out[7:1], uio_in[7:0], uio_out[7:0], uio_oe[7:0], 1'b0};
+    wire _unused = &{ena, uio_in[7:0], uio_out[7:0], uio_oe[7:0], 1'b0};
 
     pwm_gen PWM_Generador(
         .increase_duty (ui_in[0]),
         .decrease_duty (ui_in[1]),
         .divisor       (ui_in[7:2]),
-        .pwm_out       (uo_out[0]),
+        .pwm_out0      (uo_out[0]),
+        .pwm_out1      (uo_out[1]),
+        .pwm_out2      (uo_out[2]),
+        .pwm_out3      (uo_out[3]),
+        .pwm_out4      (uo_out[4]),
+        .pwm_out5      (uo_out[5]),
+        .pwm_out6      (uo_out[6]),
+        .pwm_out7      (uo_out[7]),
         .clk           (clk),
         .rst_n         (rst_n),
         .ena           (ena)
