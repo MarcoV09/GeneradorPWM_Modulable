731a13e2e7804b2e3fa8b0c550f5990bf31626bc
MarcoV09 <138732788+MarcoV09@users.noreply.github.com>
2024-11-03 19:56:39 -0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -22,19 +22,15 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
 
   // Replace tt_um_example with your module name:
   //tt_um_pwm_gen user_project (
-  tt_um_example user_project (
+  tt_um_pwm_top user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
+      .VPWR(1'b1),
+      .VGND(1'b0),
 `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
