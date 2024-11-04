1747cbd54084fb17a840a3590726a820dda53956
MarcoV09 <138732788+MarcoV09@users.noreply.github.com>
2024-11-03 22:52:20 -0700

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -29,8 +29,8 @@ module tb ();
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
+      .VPWR(1'd1),
+      .VGND(1'd0),
 `endif
 
       .ui_in  (ui_in),    // Dedicated inputs
